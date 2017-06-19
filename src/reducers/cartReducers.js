48defc1e37aac419c3adf24af0b4e18c6a51ab3e
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-06-19 11:09:52 -0400

    Add modal for checkout with calculated totals in modal and in cart.

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -3,7 +3,12 @@
 export function cartReducers(state={cart: []}, action) {
   switch(action.type) {
     case 'ADD_TO_CART':
-      return {cart: [...state, ...action.payload]}
+      return {
+        ...state,
+        cart: action.payload,
+        totalAmount: totals(action.payload).amount,
+        totalQty: totals(action.payload).qty
+      };
       break;
     case 'UPDATE_CART':
       // Create a copy of the current cart array
@@ -13,7 +18,7 @@ export function cartReducers(state={cart: []}, action) {
         function(book) {
           return book._id === action._id;
         }
-      )
+      );
       // id property was passed directly from action above so payload keyword won't be needed below
       // Update quantity of the current selected book plus the units to increase the quantity
       const newBookToUpdate = {
@@ -23,11 +28,38 @@ export function cartReducers(state={cart: []}, action) {
       // Pass the updated cart to variable cartUpdate
       let cartUpdate = [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate + 1)];
       // Then append it to the state
-      return {...state, cart: cartUpdate};
+      return {
+        ...state,
+        cart: cartUpdate,
+        totalAmount: totals(cartUpdate).amount,
+        totalQty: totals(cartUpdate).qty
+      };
       break;
     case 'DELETE_CART_ITEM':
-      return {cart: [...state, ...action.payload]}
+      return {
+        ...state,
+        cart: action.payload,
+        totalAmount: totals(action.payload).amount,
+        totalQty: totals(action.payload).qty
+      }
       break;
   }
   return state;
+}
+
+// CALCULATE TOTALS
+export function totals(payloadArr) {
+  const totalAmount = payloadArr.map(function(cartArr) {
+    return cartArr.price * cartArr.quantity;
+  }).reduce(function(a, b) {
+    return a + b;
+  }, 0); // Start summing from index 0
+
+  const totalQty = payloadArr.map(function(qty) {
+    return qty.quantity;
+  }).reduce(function(a, b) {
+    return a + b;
+  }, 0);
+
+  return {amount: totalAmount.toFixed(2), qty: totalQty};
 }
